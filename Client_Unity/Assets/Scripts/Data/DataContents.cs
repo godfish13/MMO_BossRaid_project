6d2e6967649a8a019ef28d5dd37dc9c4e4d6b7cd
godfish13 <63995325+godfish13@users.noreply.github.com>
2024-04-29 04:14:39 +0900

    20240429_minor Update

--- Client_Unity/Assets/Scripts/Data/DataContents.cs
+++ Client_Unity/Assets/Scripts/Data/DataContents.cs
@@ -62,18 +62,4 @@ public class SkillData : ILoader<int, Skill>
         return dict;
     }
 }
-
-/*[Serializable]
-public class SkillData : ILoader<int, SkillInfo>
-{
-    public List<SkillInfo> Skills = new List<SkillInfo>();     // !!!!!!중요!!!!!! JSON파일에서 받아오려는 list와 이름이 꼭!!! 같아야함
-
-    public Dictionary<int, SkillInfo> MakeDict()
-    {
-        Dictionary<int, SkillInfo> dict = new Dictionary<int, SkillInfo>();
-        foreach (SkillInfo skillInfo in Skills)
-            dict.Add(skillInfo.ClassId, skillInfo);
-        return dict;
-    }
-}*/
 #endregion
